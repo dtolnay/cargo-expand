9e6d9270e7aa54bf34f672f332e26fba1e326a43
David Tolnay <dtolnay@gmail.com>
2022-08-13 11:48:46 -0700

    Prettyplease default, rustfmt opt-in via config

--- build.rs
+++ build.rs
@@ -4,12 +4,10 @@ use std::path::PathBuf;
 
 fn main() {
     let mut version = env!("CARGO_PKG_VERSION").to_owned();
-    if cfg!(feature = "prettyplease") {
-        if let Ok(prettyplease_version) = env::var("DEP_PRETTYPLEASE01_VERSION") {
-            // TODO: Make this appear only if `--version --verbose` is used.
-            version.push_str(" + prettyplease ");
-            version.push_str(&prettyplease_version);
-        }
+    if let Ok(prettyplease_version) = env::var("DEP_PRETTYPLEASE01_VERSION") {
+        // TODO: Make this appear only if `--version --verbose` is used.
+        version.push_str(" + prettyplease ");
+        version.push_str(&prettyplease_version);
     }
 
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
