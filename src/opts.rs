1a6b14a9733384f63633a43ed566cb2ea1b6a2dd
David Tolnay <dtolnay@gmail.com>
2022-09-28 15:00:18 -0700

    Update argument parser to clap 4

--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,4 @@
-use clap::{AppSettings, Parser};
+use clap::Parser;
 use std::fmt::{self, Display};
 use std::path::PathBuf;
 use std::str::FromStr;
@@ -7,154 +7,117 @@ use syn_select::Selector;
 const VERSION: &str = include_str!(concat!(env!("OUT_DIR"), "/version"));
 
 #[derive(Parser)]
-#[clap(bin_name = "cargo", version = VERSION, author)]
+#[command(bin_name = "cargo", version = VERSION, author)]
 pub enum Opts {
     /// Show the result of macro expansion.
-    #[clap(
-        name = "expand",
-        version = VERSION,
-        author,
-        setting = AppSettings::DeriveDisplayOrder,
-        dont_collapse_args_in_usage = true
-    )]
+    #[command(name = "expand", version = VERSION, author)]
     Expand(Args),
 }
 
 #[derive(Parser, Debug)]
 pub struct Args {
     /// Space-separated list of features to activate
-    #[clap(long, value_name = "FEATURES", action)]
+    #[arg(long, value_name = "FEATURES")]
     pub features: Option<String>,
 
     /// Activate all available features
-    #[clap(long, action)]
+    #[arg(long)]
     pub all_features: bool,
 
     /// Do not activate the `default` feature
-    #[clap(long, action)]
+    #[arg(long)]
     pub no_default_features: bool,
 
     /// Expand only this package's library
-    #[clap(long, action)]
+    #[arg(long)]
     pub lib: bool,
 
     /// Expand only the specified binary
-    #[clap(
-        long,
-        value_name = "NAME",
-        min_values = 0,
-        multiple_values = false,
-        action
-    )]
+    #[arg(long, value_name = "NAME", num_args = 0..=1)]
     pub bin: Option<Option<String>>,
 
     /// Expand only the specified example
-    #[clap(
-        long,
-        value_name = "NAME",
-        min_values = 0,
-        multiple_values = false,
-        action
-    )]
+    #[arg(long, value_name = "NAME", num_args = 0..=1)]
     pub example: Option<Option<String>>,
 
     /// Expand only the specified test target
-    #[clap(
-        long,
-        value_name = "NAME",
-        min_values = 0,
-        multiple_values = false,
-        action
-    )]
+    #[arg(long, value_name = "NAME", num_args = 0..=1)]
     pub test: Option<Option<String>>,
 
     /// Include tests when expanding the lib or bin
-    #[clap(long, action)]
+    #[arg(long)]
     pub tests: bool,
 
     /// Expand only the specified bench target
-    #[clap(
-        long,
-        value_name = "NAME",
-        min_values = 0,
-        multiple_values = false,
-        action
-    )]
+    #[arg(long, value_name = "NAME", num_args = 0..=1)]
     pub bench: Option<Option<String>>,
 
     /// Target triple which compiles will be for
-    #[clap(long, value_name = "TARGET", action)]
+    #[arg(long, value_name = "TARGET")]
     pub target: Option<String>,
 
     /// Directory for all generated artifacts
-    #[clap(long, value_name = "DIRECTORY", action)]
+    #[arg(long, value_name = "DIRECTORY")]
     pub target_dir: Option<PathBuf>,
 
     /// Path to Cargo.toml
-    #[clap(long, value_name = "PATH", action)]
+    #[arg(long, value_name = "PATH")]
     pub manifest_path: Option<PathBuf>,
 
     /// Package to expand
-    #[clap(
-        short,
-        long,
-        value_name = "SPEC",
-        min_values = 0,
-        multiple_values = false,
-        action
-    )]
+    #[arg(short, long, value_name = "SPEC", num_args = 0..=1)]
     pub package: Option<Option<String>>,
 
     /// Build artifacts in release mode, with optimizations
-    #[clap(long, action)]
+    #[arg(long)]
     pub release: bool,
 
     /// Build artifacts with the specified profile
-    #[clap(long, value_name = "PROFILE-NAME", action)]
+    #[arg(long, value_name = "PROFILE-NAME")]
     pub profile: Option<String>,
 
     /// Number of parallel jobs, defaults to # of CPUs
-    #[clap(short, long, value_name = "N", action)]
+    #[arg(short, long, value_name = "N")]
     pub jobs: Option<u64>,
 
     /// Print command lines as they are executed
-    #[clap(long, action)]
+    #[arg(long)]
     pub verbose: bool,
 
     /// Coloring: auto, always, never
-    #[clap(long, value_name = "WHEN", action)]
+    #[arg(long, value_name = "WHEN")]
     pub color: Option<Coloring>,
 
     /// Require Cargo.lock and cache are up to date
-    #[clap(long, action)]
+    #[arg(long)]
     pub frozen: bool,
 
     /// Require Cargo.lock is up to date
-    #[clap(long, action)]
+    #[arg(long)]
     pub locked: bool,
 
     /// Run without accessing the network
-    #[clap(long, action)]
+    #[arg(long)]
     pub offline: bool,
 
     /// Unstable (nightly-only) flags to Cargo
-    #[clap(short = 'Z', value_name = "FLAG", action)]
+    #[arg(short = 'Z', value_name = "FLAG")]
     pub unstable_flags: Vec<String>,
 
     /// Do not attempt to run rustfmt
-    #[clap(long, action)]
+    #[arg(long)]
     pub ugly: bool,
 
     /// Select syntax highlighting theme
-    #[clap(long, value_name = "NAME", action)]
+    #[arg(long, value_name = "NAME")]
     pub theme: Option<String>,
 
     /// Print available syntax highlighting theme names
-    #[clap(long, action)]
+    #[arg(long)]
     pub themes: bool,
 
     /// Local path to module or other named item to expand, e.g. os::unix::ffi
-    #[clap(value_name = "ITEM", value_parser = parse_selector)]
+    #[arg(value_name = "ITEM", value_parser = parse_selector)]
     pub item: Option<Selector>,
 }
 
