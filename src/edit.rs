3828db275e9187710f5185bea4193b622e32eb3a
David Tolnay <dtolnay@gmail.com>
2023-03-22 17:39:00 -0700

    Update to syn 2

--- src/edit.rs
+++ src/edit.rs
@@ -1,11 +1,11 @@
 use syn::visit_mut::{self, VisitMut};
 use syn::{
-    Attribute, Block, Expr, ExprArray, ExprAssign, ExprAssignOp, ExprAsync, ExprAwait, ExprBinary,
-    ExprBlock, ExprBox, ExprBreak, ExprCall, ExprCast, ExprClosure, ExprContinue, ExprField,
-    ExprForLoop, ExprGroup, ExprIf, ExprIndex, ExprLet, ExprLit, ExprLoop, ExprMacro, ExprMatch,
+    Attribute, Block, Expr, ExprArray, ExprAssign, ExprAsync, ExprAwait, ExprBinary, ExprBlock,
+    ExprBreak, ExprCall, ExprCast, ExprClosure, ExprConst, ExprContinue, ExprField, ExprForLoop,
+    ExprGroup, ExprIf, ExprIndex, ExprInfer, ExprLet, ExprLit, ExprLoop, ExprMacro, ExprMatch,
     ExprMethodCall, ExprParen, ExprPath, ExprRange, ExprReference, ExprRepeat, ExprReturn,
-    ExprStruct, ExprTry, ExprTryBlock, ExprTuple, ExprType, ExprUnary, ExprUnsafe, ExprWhile,
-    ExprYield, File, Item, ItemMod, Stmt,
+    ExprStruct, ExprTry, ExprTryBlock, ExprTuple, ExprUnary, ExprUnsafe, ExprWhile, ExprYield,
+    File, Item, ItemMod, Stmt,
 };
 
 pub fn sanitize(syntax_tree: &mut File) {
@@ -36,12 +36,13 @@ impl VisitMut for Sanitize {
     fn visit_stmt_mut(&mut self, i: &mut Stmt) {
         match i {
             Stmt::Local(local) => remove_doc_attributes(&mut local.attrs),
-            Stmt::Expr(e) | Stmt::Semi(e, _) => {
+            Stmt::Expr(e, _semi) => {
                 if let Some(attrs) = attrs_mut(e) {
                     remove_doc_attributes(attrs);
                 }
             }
             Stmt::Item(_) => {}
+            Stmt::Macro(mac) => remove_doc_attributes(&mut mac.attrs),
         }
         visit_mut::visit_stmt_mut(self, i);
     }
@@ -55,29 +56,29 @@ fn remove_macro_rules_from_vec_item(items: &mut Vec<Item>) {
 }
 
 fn remove_doc_attributes(attrs: &mut Vec<Attribute>) {
-    attrs.retain(|attr| !attr.path.is_ident("doc"));
+    attrs.retain(|attr| !attr.path().is_ident("doc"));
 }
 
 fn attrs_mut(e: &mut Expr) -> Option<&mut Vec<Attribute>> {
     match e {
         Expr::Array(ExprArray { attrs, .. })
         | Expr::Assign(ExprAssign { attrs, .. })
-        | Expr::AssignOp(ExprAssignOp { attrs, .. })
         | Expr::Async(ExprAsync { attrs, .. })
         | Expr::Await(ExprAwait { attrs, .. })
         | Expr::Binary(ExprBinary { attrs, .. })
         | Expr::Block(ExprBlock { attrs, .. })
-        | Expr::Box(ExprBox { attrs, .. })
         | Expr::Break(ExprBreak { attrs, .. })
         | Expr::Call(ExprCall { attrs, .. })
         | Expr::Cast(ExprCast { attrs, .. })
         | Expr::Closure(ExprClosure { attrs, .. })
+        | Expr::Const(ExprConst { attrs, .. })
         | Expr::Continue(ExprContinue { attrs, .. })
         | Expr::Field(ExprField { attrs, .. })
         | Expr::ForLoop(ExprForLoop { attrs, .. })
         | Expr::Group(ExprGroup { attrs, .. })
         | Expr::If(ExprIf { attrs, .. })
         | Expr::Index(ExprIndex { attrs, .. })
+        | Expr::Infer(ExprInfer { attrs, .. })
         | Expr::Let(ExprLet { attrs, .. })
         | Expr::Lit(ExprLit { attrs, .. })
         | Expr::Loop(ExprLoop { attrs, .. })
@@ -94,7 +95,6 @@ fn attrs_mut(e: &mut Expr) -> Option<&mut Vec<Attribute>> {
         | Expr::Try(ExprTry { attrs, .. })
         | Expr::TryBlock(ExprTryBlock { attrs, .. })
         | Expr::Tuple(ExprTuple { attrs, .. })
-        | Expr::Type(ExprType { attrs, .. })
         | Expr::Unary(ExprUnary { attrs, .. })
         | Expr::Unsafe(ExprUnsafe { attrs, .. })
         | Expr::While(ExprWhile { attrs, .. })
