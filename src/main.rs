7f4107d81048fbb406e8834df0138694af7d11ac
David Tolnay <dtolnay@gmail.com>
2023-07-16 13:49:17 -0700

    Ignore module_name_repetitions pedantic clippy lint
        warning: item name ends with its containing module's name
         --> src/manifest.rs:9:12
          |
        9 | pub struct CargoManifest {
          |            ^^^^^^^^^^^^^
          |
          = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#module_name_repetitions
          = note: `-W clippy::module-name-repetitions` implied by `-W clippy::pedantic`

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
     clippy::manual_strip,
     clippy::match_like_matches_macro,
     clippy::match_same_arms, // https://github.com/rust-lang/rust-clippy/issues/10327
+    clippy::module_name_repetitions,
     clippy::needless_borrow, // https://github.com/rust-lang/rust-clippy/issues/9710
     clippy::needless_pass_by_value,
     clippy::non_ascii_literal,
