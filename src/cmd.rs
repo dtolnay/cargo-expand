275c52e29ea4dc7779f83e6a58c980df1119611a
David Tolnay <dtolnay@gmail.com>
2024-04-06 09:39:36 -0700

    Pass flags as single arg where possible

--- src/cmd.rs
+++ src/cmd.rs
@@ -11,8 +11,28 @@ impl CommandArgs {
         CommandArgs { args: Vec::new() }
     }
 
-    pub fn arg<S: AsRef<OsStr>>(&mut self, arg: S) {
+    pub fn arg<S>(&mut self, arg: S)
+    where
+        S: AsRef<OsStr>,
+    {
         self.args.push(arg.as_ref().to_owned());
+    }
+
+    pub fn flag_value<K, V>(&mut self, k: K, v: V)
+    where
+        K: AsRef<OsStr>,
+        V: AsRef<OsStr>,
+    {
+        let k = k.as_ref();
+        let v = v.as_ref();
+        if let Some(k) = k.to_str() {
+            if let Some(v) = v.to_str() {
+                self.arg(format!("{}={}", k, v));
+                return;
+            }
+        }
+        self.arg(k);
+        self.arg(v);
     }
 
     pub fn args<I, S>(&mut self, args: I)
