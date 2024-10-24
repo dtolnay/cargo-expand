09f73c8182fd94400a1c1c4e02fa77e526f6b515
David Tolnay <dtolnay@gmail.com>
2024-10-23 22:53:30 -0700

    Load custom bat theme assets

---
+++ src/assets.rs
@@ -0,0 +1,26 @@
+use crate::error::Result;
+use etcetera::BaseStrategy as _;
+use std::env;
+use std::path::PathBuf;
+use std::str;
+
+pub const BAT_VERSION: &str = {
+    let mut bytes = include_str!("bat.version").as_bytes();
+    while let [rest @ .., b'\n' | b'\r'] = bytes {
+        bytes = rest;
+    }
+    if let Ok(version) = str::from_utf8(bytes) {
+        version
+    } else {
+        panic!()
+    }
+};
+
+pub fn cache_dir() -> Result<PathBuf> {
+    if let Some(cache_dir) = env::var_os("BAT_CACHE_PATH") {
+        return Ok(PathBuf::from(cache_dir));
+    }
+
+    let basedirs = etcetera::choose_base_strategy()?;
+    Ok(basedirs.cache_dir().join("bat"))
+}
