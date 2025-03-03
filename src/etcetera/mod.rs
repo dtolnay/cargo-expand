69f8fad695ca20f6b72e48be49dc409fa0beb7ae
David Tolnay <dtolnay@gmail.com>
2025-03-02 22:23:42 -0800

    Inline etcetera modules into a single file

--- src/etcetera/mod.rs
+++
@@ -1,9 +0,0 @@
-use crate::error::{Error, Result};
-
-pub mod base_strategy;
-
-pub use base_strategy::{choose_base_strategy, BaseStrategy};
-
-pub fn home_dir() -> Result<std::path::PathBuf> {
-    home::home_dir().ok_or(Error::HomeDir)
-}
