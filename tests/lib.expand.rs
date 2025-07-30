583dcb62deee38cb5bb2487d621b96a6592b7f26
David Tolnay <dtolnay@gmail.com>
2025-07-29 22:30:20 -0700

    Update lib.expand.rs to nightly-2025-07-30

--- tests/lib.expand.rs
+++ tests/lib.expand.rs
@@ -1,8 +1,8 @@
 #![feature(prelude_import)]
 //! Test
-#[prelude_import]
-use std::prelude::rust_2021::*;
 #[macro_use]
 extern crate std;
+#[prelude_import]
+use std::prelude::rust_2021::*;
 /// Test
 pub fn test() {}
